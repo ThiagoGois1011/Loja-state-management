854004f26eef96473d4c4c7a4e334f51219a8a5c
Thiago <thiagogoes1011@gmail.com>
2024-01-31 09:44:22 -0300

    salvando

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import {createContext} from 'react';
 import './App.css';
 import Header from './components/Header'
 import Content from './components/Content'
 
+let listaProdutos = [{
+  src: "https://images.tcdn.com.br/img/img_prod/1128823/brinquedo_carro_de_policial_com_luz_e_som_e_friccao_1_16_000762_3603_2_385fa017eafcc7add45724e1a75e2b54.jpg",
+  titulo: "Carro da Polícia de Brinquedo",
+  preco: "R$ 20.00",
+  id: "1"
+}];
+
+export const ApiContext = createContext(listaProdutos);
+
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Content/>
+      <ApiContext.Provider value={listaProdutos}>
+        <Header/>
+        <Content/>
+      </ApiContext.Provider>
     </div>
   );
 }
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,14 +1,12 @@
 import Produto from "./Produto";
 import style from "./moduleCss/content.module.css";
-
-let listaProdutos = [{
-    src: "https://images.tcdn.com.br/img/img_prod/1128823/brinquedo_carro_de_policial_com_luz_e_som_e_friccao_1_16_000762_3603_2_385fa017eafcc7add45724e1a75e2b54.jpg",
-    titulo: "Carro da Polícia de Brinquedo",
-    preco: "R$ 20.00",
-    id: "1"
-}];
+import { useContext} from "react";
+import {ApiContext} from "../App"
 
 function Content(){
+    const produtosList = useContext(ApiContext);
+    console.log(produtosList);
+
     return(
         <section className={style.section}>
             <div className={style.div_content}>
@@ -20,7 +18,7 @@ function Content(){
                     </div>
                 </div>
                 <div>
-                    {listaProdutos.map(produto => <Produto key={produto.id} src={produto.src} titulo={produto.titulo} preco={produto.preco}/>)}
+                    {produtosList.map(produto => <Produto key={produto.id} src={produto.src} titulo={produto.titulo} preco={produto.preco}/>)}
                 </div>
             </div>
         </section>
